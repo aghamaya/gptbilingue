5acae92e89c5d348e10425b845c77a635d6bd360
Hamaya AG ABDOULAYE <hamaya.ag@graduateinstitute.ch>
2023-09-18 00:16:18 +0000

    Mise à jour de l'app

--- gptbilingue_app.py
+++ gptbilingue_app.py
@@ -9,15 +9,16 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Fonction pour initialiser ou récupérer l'historique des messages de la session
 def get_message_history():
     if 'message_history' not in st.session_state:
-        st.session_state.message_history = [{'role': 'system', 'content': "Tu es un assistant intelligent capable de répondre à des questions sur l'enseignement bilingue dans les pays d'Afrique francophone"}]
+        st.session_state.message_history = [{'role': 'system', 'content': "Tu es un assistant intelligent capable de répondre à des questions sur l'enseignement bilingue dans les pays d'Afrique francophone, particuliérement les pays partenaire de l'initiative Ecole et langues nationales en Afrique (ELAN-Afrique) mise en oeuvre par l'IFEF"}]
     return st.session_state.message_history
 
 def main():
-    st.markdown("<h1 style='text-align: center; color: navy;'>Assistant Intelligent pour l'Enseignement Bilingue</h1>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center; color: green;'>Auteur : Hamaya AG-ABDOULAYE</h6>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: navy;'>Assistant Intelligent programmé pour repondre à vos questions sur le Bilinguisme</h2>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: center; color: green;'>Auteur : Hamaya AG-ABDOULAYE</h5>", unsafe_allow_html=True)
+    st.markdown("<h6 style='color: blue;'>Ce chatbot est réglé sur le modéle gpt-3.5-turbo de ChatGPT, les prédictions peuvent souvent produire des informations inexactes sur des personnes, des lieux ou des faits. Son assistant a été programmé sur la situation de l'enseignement bilingue dans les pays Francophone d'Afrique, le plus souvant partenaires du programme ELAN. N'hésitez pas à rebondir sur les reponses prédites, le relancé pour recadrer et ainsi tiré profit du meilleir de cet assistant intelligent</h6>", unsafe_allow_html=True)
 
     st.sidebar.header("PARAMETRES")
-    slider1 = st.sidebar.slider("Max tokens", min_value=100, max_value=1000, value=400, step=1)
+    slider1 = st.sidebar.slider("Max tokens", min_value=80, max_value=500, value=270, step=10)
     slider2 = st.sidebar.slider("Reglage de la temperature", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
     
     
